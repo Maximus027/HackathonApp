28a72fb19608130faf5f32fa6cde0f96adaf99c5
Max Rosado <max.a.rosado@gmail.com>
2017-02-18 14:09:43 -0500

    added location activity, got a functioning map, on the way to set up a marker for current location.

---
+++ app/src/main/java/nyc/c4q/maxrosado/hackathonapp/LocationActivity.java
@@ -0,0 +1,38 @@
+package nyc.c4q.maxrosado.hackathonapp;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.google.android.gms.maps.CameraUpdate;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+
+public class LocationActivity extends AppCompatActivity implements OnMapReadyCallback {
+    private GoogleMap mainGoogleMap;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_location);
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mainGoogleMap = googleMap;
+        gotToLocationZoom(40.739194, -73.930890, 15);
+//        googleMap.addMarker(new MarkerOptions().position(40.739194, -73.930890)
+//                .title("Name of location"));
+    }
+
+    private void gotToLocationZoom(double lat, double lng, float zoom) {
+        LatLng ll = new LatLng(lat, lng);
+        CameraUpdate update = CameraUpdateFactory.newLatLngZoom(ll, zoom);
+        mainGoogleMap.moveCamera(update);
+    }
+}
