1ce4cc1ed0ab9f23ccc49476461423085f1ba36c
qbergen <queenabergen@gmail.com>
2017-02-18 14:04:53 -0500

    sports view holder and recyclerview

--- app/src/main/java/nyc/c4q/maxrosado/hackathonapp/HomeActivity.java
+++ app/src/main/java/nyc/c4q/maxrosado/hackathonapp/HomeActivity.java
@@ -2,12 +2,26 @@ package nyc.c4q.maxrosado.hackathonapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+import nyc.c4q.maxrosado.hackathonapp.Adapters.BasketballAdapter;
 
 public class HomeActivity extends AppCompatActivity {
+    private RecyclerView recyclerView;
+    private LinearLayoutManager linearLayoutManager;
+    BasketballAdapter basketballAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home);
+        setContentView(R.layout.sportslandingpage);
+
+        recyclerView = (RecyclerView) findViewById(R.id.sportsRecView);
+        linearLayoutManager = new LinearLayoutManager(getApplicationContext(), LinearLayoutManager.VERTICAL, false);
+        basketballAdapter = new BasketballAdapter();
+        recyclerView.setLayoutManager(linearLayoutManager);
+        recyclerView.setAdapter(basketballAdapter);
+
     }
 }
