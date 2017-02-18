c2fa2b699d27c693e23dccb6d9dd54d1a98d58bb
exmelenez <exmelendez@gmail.com>
2017-02-18 13:46:10 -0500

    first commit-login screen/activity

---
+++ app/src/main/java/nyc/c4q/maxrosado/hackathonapp/login/LoginCreate.java
@@ -0,0 +1,15 @@
+package nyc.c4q.maxrosado.hackathonapp.login;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import nyc.c4q.maxrosado.hackathonapp.R;
+
+public class LoginCreate extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login_create);
+    }
+}
