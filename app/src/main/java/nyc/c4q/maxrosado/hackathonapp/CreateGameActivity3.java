46c3d1f6f9d5dd2714432f7b732ac7db4225c53e
qbergen <queenabergen@gmail.com>
2017-02-21 02:55:13 -0500

    update to page 4, working code

--- app/src/main/java/nyc/c4q/maxrosado/hackathonapp/CreateGameActivity3.java
+++ app/src/main/java/nyc/c4q/maxrosado/hackathonapp/CreateGameActivity3.java
@@ -1,18 +1,29 @@
 package nyc.c4q.maxrosado.hackathonapp;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 /**
  * Created by queenabergen on 2/21/17.
  */
 public class CreateGameActivity3 extends Activity {
-
+    Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.creategamepage3);
+        button = (Button) findViewById(R.id.continuebtn);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), CreateGameActivity4.class);
+                startActivity(intent);
+            }
+        });
 
     }
 }
---
+++ app/src/main/java/nyc/c4q/maxrosado/hackathonapp/CreateGameActivity4.java
@@ -0,0 +1,13 @@
+package nyc.c4q.maxrosado.hackathonapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CreateGameActivity4 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.creategamepage4);
+    }
+}
